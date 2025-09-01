715e5fbe5cfab1efd67cf6bbf7ca6d4eefb1f4c7
Davi <andrielysom@gmail.com>
2025-09-01 11:26:19 -0300

    vai

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const mercadopago = require('mercadopago');
+const { MercadoPagoConfig, Preference } = require('mercadopago');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// 👉 Coloca o token direto aqui
-mercadopago.configure({
-  access_token: 'APP_USR-6806578338398236-090109-52cf5ad78f0a4d300d432a1ed5108fa2-2659262227'
+// 👉 Novo jeito de configurar
+const client = new MercadoPagoConfig({
+  accessToken: 'APP_USR-6806578338398236-090109-52cf5ad78f0a4d300d432a1ed5108fa2-2659262227'
 });
 
 app.post('/criar-preferencia', async (req, res) => {
@@ -30,8 +30,9 @@ app.post('/criar-preferencia', async (req, res) => {
   };
 
   try {
-    const response = await mercadopago.preferences.create(preference);
-    res.json({ init_point: response.body.init_point });
+    const preferenceClient = new Preference(client);
+    const response = await preferenceClient.create(preference);
+    res.json({ init_point: response.init_point });
   } catch (error) {
     console.error('Erro ao criar preferência:', error);
     res.status(500).json({ error: 'Erro ao criar preferência' });
