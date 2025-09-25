a5191ab1b605f7c86148b075ce528c8b518f8595
Davi <andrielysom@gmail.com>
2025-09-25 17:31:10 -0300

    vai

--- public/script.js
+++ public/script.js
@@ -324,16 +324,17 @@ document.getElementById("adicionarObjetivo").addEventListener("click", () => {
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelectorAll(".mesAno").forEach(input => {
     if (!input._flatpickr) {
-      flatpickr(input, {
-        locale: flatpickr.l10ns.pt,
-        plugins: [
-          new monthSelectPlugin({
-            shorthand: false,
-            dateFormat: "m/Y",
-            altFormat: "F Y"
-          })
-        ]
-      });
+flatpickr(input, {
+  locale: flatpickr.l10ns.pt,
+  disableMobile: true, // ← ESSENCIAL
+  plugins: [
+    new monthSelectPlugin({
+      shorthand: false,
+      dateFormat: "m/Y",
+      altFormat: "F Y"
+    })
+  ]
+});
     }
   });
 });
