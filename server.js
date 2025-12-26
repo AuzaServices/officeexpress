d930ffc69347790d21a3f41c0976e16b17f45f86
Davi <andrielysom@gmail.com>
2025-12-26 16:20:39 -0300

    vai

--- server.js
+++ server.js
@@ -21,7 +21,6 @@ app.use(bodyParser.text({ type: 'text/plain' }));
 
 // 🔁 Conexão com MySQL
 require('dotenv').config();
-const mysql = require('mysql2');
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
