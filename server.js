8e7f42fe6e4becf8a1d6e3a213f390e7ba25f30f
Davi <andrielysom@gmail.com>
2026-01-28 21:45:41 -0300

    vai

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const session = require('express-session');
 const mysql = require('mysql2/promise');
 const PDFDocument = require('pdfkit');
 const bodyParser = require('body-parser');
