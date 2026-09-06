6b36616178dd876714b76aedcf810adeda698092
Davi <andrielysom@gmail.com>
2026-09-06 00:51:26 -0300

    vai

--- _scan.js
+++
@@ -1,3 +0,0 @@
-const s = require('fs').readFileSync('c:/Users/andri/OneDrive/Desktop/Sites/OfficeExpress/public/companies.html', 'utf8');
-let i = s.indexOf('id="viewForm"');
-console.log(s.slice(i, i + 3200));
